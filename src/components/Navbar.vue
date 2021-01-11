587fa48fa676fac03848fcf5e0d5625fa9a7e684
Phong Tran <phonghtran@gmail.com>
2021-01-10 22:33:51 -0500

    fix nav bar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,20 +1,11 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light" fixed="top">
-    <a class="navbar-brand" href="#">PFCRM</a>
-    <button
-      class="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarSupportedContent"
-      aria-controls="navbarSupportedContent"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
+  <b-navbar toggleable="sm" sticky="true">
+    <b-navbar-brand href="#">PFCRM</b-navbar-brand>
 
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav mr-auto">
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
         <li class="nav-item">
           <router-link class="nav-link" to="/">Home</router-link>
         </li>
@@ -38,20 +29,22 @@
         <li class="nav-item">
           <a @click.prevent="signOut" class="nav-link" href="#">Sign out</a>
         </li>
-      </ul>
+      </b-navbar-nav>
 
-      <div>
-        <input
-          type="checkbox"
-          id="scrambleNames"
-          name="scrambleNames"
-          v-model="scrambleNamesFlag"
-          @change="switchScrambleNames"
-        />
-        <label for="scrambleNames">Scramble Names</label>
-      </div>
-    </div>
-  </nav>
+      <b-navbar-nav class="ml-auto ">
+        <div class="d-flex align-items-center">
+          <input
+            type="checkbox"
+            id="scrambleNames"
+            name="scrambleNames"
+            v-model="scrambleNamesFlag"
+            @change="switchScrambleNames"
+          />
+          <label for="scrambleNames">Scramble Names</label>
+        </div>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 </template>
 
 <script>
@@ -88,3 +81,4 @@
     },
   };
 </script>
+<style lang="scss"></style>
